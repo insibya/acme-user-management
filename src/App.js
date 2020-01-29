176b830aa62dbc296f3c3336671cb8959e8cd92b
Nathan Roy <nathanmroy@yahoo.com>
2020-01-28 20:33:13 -0800

    phase one complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import qs from 'qs';
 import './App.css';
 
+const API = 'https://acme-users-api-rev.herokuapp.com/';
+
+const Users = () => {
+	return <hr />;
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const getHash = () => {
+		return window.location.hash.slice(1);
+	};
+	const [ params, setParams ] = useState(qs.parse(getHash()));
+	const [ users, setUsers ] = useState([]);
+
+	useEffect(() => {
+		window.addEventListener('hashchange', () => {
+			setParams(qs.parse(getHash()));
+		});
+		setParams(qs.parse(getHash()));
+	}, []);
+
+	const view = params.view;
+	return (
+		<div className="App">
+			<nav>
+				<a href="#" className={!view ? 'selected' : ''}>
+					Home
+				</a>
+				<a href="#view=users" className={view === 'users' ? 'selected' : ''}>
+					Users
+				</a>
+			</nav>
+			{view === 'users' && <Users users={users} />}
+		</div>
+	);
 }
 
 export default App;
